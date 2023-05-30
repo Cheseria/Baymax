b1d7a3943db6ea5621364185f3b3f70b25df3907
Cheseria <133724412+Cheseria@users.noreply.github.com>
2023-05-30 21:34:59 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-include "config.php";
+include "src/config.php";
 
 session_start();
 
 // Check if the user is not logged in
 if (!isset($_SESSION['username'])) {
-    header('Location: login.php');
+    header('Location: src/login.php');
     exit();
 }
 
@@ -37,16 +37,16 @@ if ($rows > 0) {
 	<head>
 		<meta charset="utf-8">
 		<title>Event Calendar</title>
-		<link href="css/style.css" rel="stylesheet" type="text/css">
-		<link href="css/calendar.css" rel="stylesheet" type="text/css">
-		<link href="css/dynamic_styles.css" rel="stylesheet" type="text/css">
+		<link href="src/css/style.css" rel="stylesheet" type="text/css">
+		<link href="src/css/calendar.css" rel="stylesheet" type="text/css">
+		<link href="src/css/dynamic_styles.css" rel="stylesheet" type="text/css">
 		
 	</head>
 	<body> 
         <div class='head'>
             <h1>Welcome, <?php echo $_SESSION['username'];?>!</h1>
 
-			<button onclick="window.location.href = 'logout.php';">Logout</button>
+			<button onclick="window.location.href = 'src/logout.php';">Logout</button>
         </div>
 		
 		<div class="content home">	
