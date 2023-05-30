caebf36419c2e164b7441a472f124622d4018e3b
Cheseria <133724412+Cheseria@users.noreply.github.com>
2023-05-30 21:33:56 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,22 +1,34 @@
 <?php
+include "config.php";
 
 session_start();
 
 // Check if the user is not logged in
 if (!isset($_SESSION['username'])) {
-    header('Location: src/login.php');
+    header('Location: login.php');
     exit();
-}
-
-if (isset($_POST['logout'])) {
-    session_destroy(); // Destroy the session
-    header('Location: src/login.php'); // Redirect to login page
-    exit;
 }
 
 include 'calendar.php';
 $currentdate =  date('Y-m-d');
 $calendar = new Calendar($currentdate);
+
+$UserID = $_SESSION['UserID'];
+$sql = "SELECT * FROM event WHERE UserID = $UserID";
+$result =  $connection->query($sql);
+$rows = mysqli_num_rows($result);
+
+if ($rows > 0) {
+	// Loop through the rows and add the events to the calendar
+	while ($row = $result->fetch_assoc()) {
+		$EventName = $row['EventName'];
+		$EventDate = $row['EventDate'];
+		$CategoryID = $row['CategoryID'];
+
+		// Add the event to the calendar
+		$calendar->add_event($EventName, $EventDate, 1, $CategoryID);
+	}
+}
 ?>
 
 
@@ -25,23 +37,23 @@ $calendar = new Calendar($currentdate);
 	<head>
 		<meta charset="utf-8">
 		<title>Event Calendar</title>
-		<link href="src/css/style.css" rel="stylesheet" type="text/css">
-		<link href="src/css/calendar.css" rel="stylesheet" type="text/css">
-		
+		<link href="css/style.css" rel="stylesheet" type="text/css">
+		<link href="css/calendar.css" rel="stylesheet" type="text/css">
+		<link href="css/dynamic_styles.css" rel="stylesheet" type="text/css">
 		
 	</head>
 	<body> 
         <div class='head'>
             <h1>Welcome, <?php echo $_SESSION['username'];?>!</h1>
-            <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-            <input type="submit" name="logout" value="Logout">
+
+			<button onclick="window.location.href = 'logout.php';">Logout</button>
         </div>
+		
 		<div class="content home">	
-			<?=$calendar?>
+			<div><?=$calendar?></div>
 		</div>
 
-		
-		<script src="src/script.js"></script>
+		<script src="script.js"></script>
 	</body>
 	
 	
